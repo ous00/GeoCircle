620f59365fb77adbd8c41a7a12deee7438631565
ous00 <ousoo@ymail.com>
2011-08-30 21:27:12 +0800

    modified:   content/views.py 	modified:   templates/form.html

--- GeoCircle/src/GeoCircle/content/views.py
+++ GeoCircle/src/GeoCircle/content/views.py
@@ -2,11 +2,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from GeoCircle.content.models import Content
+from djangotoolbox.fields import BlobField
 
 def addContent(request):
-    message = request.POST.get('message', '')
-#    message = request.GET.get('message','')
-    if not len(message) == 0: 
-        return HttpResponse(u'you sumbited %s' % message )
+    title = request.POST.get('title', '')
+#    message = request.GET.get('message','')    
+    if not len(title) == 0: 
+        blob = BlobField(request.POST.get('messesage', ''))
+        content = Content.objects.create(description=title, author_id=1, store=blob)
+        content.save()
+        
+        return HttpResponse(u'you sumbited %s' % title )
     else:
         return render_to_response('form.html')
